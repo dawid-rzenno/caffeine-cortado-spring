f8c0a03ba71eb105ad39493dd60bd1653a1f7758
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-12-09 15:38:35 +0100

    Clean up files and their location

--- src/main/java/com/caffeine/cortado/user/UserRepository.java
+++ src/main/java/com/caffeine/cortado/user/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-  public User findByUsername(String login);
+  public User findByUsername(String username);
 }
