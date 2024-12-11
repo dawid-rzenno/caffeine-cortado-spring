4a82da91216723cb4752919b49560758e4cc5793
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-12-11 18:59:18 +0100

    Rename for better readability

--- src/main/java/com/caffeine/cortado/auth/WebMvcConfigurerImpl.java
+++ src/main/java/com/caffeine/cortado/auth/WebMvcConfigurerImpl.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvc implements WebMvcConfigurer {
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
